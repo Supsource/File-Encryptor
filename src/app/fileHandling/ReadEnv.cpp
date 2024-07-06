5ae7ed2a4f3d027ce2e2e742c84dd60fd28d3ddc
Supriyo <supriyomalstartup@gmail.com>
2024-07-06 15:20:47 +0530

    feat: Implement .env file support and reading
    - Added support for environment variables using a .env file
    - Implemented functionality to read and load environment variables
    - Updated configuration to use environment variables where applicable
    - Improved security by externalizing sensitive information
    - Updated documentation to include instructions for .env setup

---
+++ src/app/fileHandling/ReadEnv.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<sstream>
+#include "IO.hpp"
+
+class ReadEnv{
+    public:
+        std::string getend(){
+            std::string env_path = ".env";
+            IO io(env_path);
+            std::fstream f_stream = io.getFileStream();
+            std::stringstream buffer;
+            buffer << f_stream.rdbuf();
+            std::string content = buffer.str();
+            return content;
+        }
+};
